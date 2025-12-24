de4c374346be79136747e7d23f693ccc6e18e173
[iharshkumar] <[srivastavaharsh1108@gmail.com]>
2025-12-24 19:10:39 +0530

    Navbar edit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <div className="navbar-brand">Ecommerce Pro</div>
+        <nav className="navbar bg-gray-800 text-white p-4 rounded-md">
+            <div className="navbar-brand ">Ecommerce Website</div>
             <ul className="navbar-links">
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Products</a></li>
